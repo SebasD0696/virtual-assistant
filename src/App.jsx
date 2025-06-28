59eaf9432c3788f0c4e0b1c0ec38dae7141189c8
SebasD0696 <sebasdiaz0696@gmail.com>
2025-06-28 18:04:11 -0500

    feat: ajuste avatar

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,34 @@ export default function App() {
 
   return (
     <>
-      <Canvas camera={{ position: [0, 1.5, 3] }}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[2, 2, 2]} />
-        <Suspense fallback={null}>
-          <Avatar text={text} />
-          <OrbitControls />
-        </Suspense>
-      </Canvas>
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '20px',
+          left: '20px',
+          width: '250px',
+          height: '300px',
+          background: 'rgba(0, 0, 0, 0.7)',
+          borderRadius: '16px',
+          overflow: 'hidden',
+          boxShadow: '0 0 20px rgba(0,0,0,0.5)',
+        }}
+      >
+        <Canvas 
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          camera={{ position: [0 , 1.5, 3.5] }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[2, 2, 2]} />
+          <Suspense fallback={null}>
+            <Avatar text={text} />
+            <OrbitControls />
+          </Suspense>
+        </Canvas>
+      </div>
+
       <ChatInput setText={setText} />
     </>
   )
